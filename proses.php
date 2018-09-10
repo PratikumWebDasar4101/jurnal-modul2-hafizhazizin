deffc102bd9c2a1a53dd1d77d5376f4e6aa2040c
Hafizh 'Azizi Nasution <42725297+hafizhazizin@users.noreply.github.com>
2018-09-10 09:43:10 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,4 +1,18 @@
 <?php 
-//Tuliskan kode anda di bawah ini
-
+$a = $_GET['a'];
+	function rekursif($z,$x){
+	 	if ($z > 0) {
+	 		if ($x > 0) {
+	 			echo "* ";
+	 			$x--;
+	 			rekursif($z,$x);
+	 		}	
+	 		else{
+	 			echo "<br>";
+	 			$z--;
+	 			rekursif($z,$z);
+	 		}
+	 	}
+	 }
+	rekursif ($a, $a);
 ?>
