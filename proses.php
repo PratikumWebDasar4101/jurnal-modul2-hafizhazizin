d1a3131570831bf0602af802ba0a22fece934cce
Hafizh 'Azizi Nasution <42725297+hafizhazizin@users.noreply.github.com>
2018-09-10 10:04:02 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,5 +1,9 @@
 <?php 
+
 $a = $_GET['a'];
+$genap =array();
+$ganjil =array();
+
 	function rekursif($z,$x){
 	 	if ($z > 0) {
 	 		if ($x > 0) {
@@ -15,4 +19,29 @@ $a = $_GET['a'];
 	 	}
 	 }
 	rekursif ($a, $a);
-?>
+//==================================================
+	echo "<br>";
+	for ($i=0; $i <$a ; $i++) { 
+		if ($i % 2 == 0) {
+			array_push($genap, $i);
+		}
+		else{
+			array_push($ganjil, $i);
+		}
+	}
+
+	echo "genap :";
+	for ($i=0; $i< count($genap) ; $i++) { 
+		echo $genap[$i];
+
+	}
+	echo "<br>";
+	echo "ganjil :";
+	for ($i=0; $i< count($ganjil) ; $i++) { 
+		echo $ganjil[$i];
+	}
+	echo "<br>";
+	echo" Jumlah Deret Genap  :".count($genap);
+	echo "<br>";
+	echo" Jumlah Deret Ganjil :".count($ganjil);
+ ?>
